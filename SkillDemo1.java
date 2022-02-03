5a1e907cb8a41ecb35e2dbbb8cfa37d891f656fc
ghwusd <ghwu@ucsd.edu>
2022-02-02 18:12:04 -0800

    added lib and tester

--- SkillDemo1.java
+++ SkillDemo1.java
@@ -1,1 +1,5 @@
-
+public class SkillDemo1 {
+    public static int multiplication (int x, int y) {
+        return x * y;
+    }
+}
---
+++ SkillDemo1Tester.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemo1Tester {
+    @Test
+    public void multiplication() {
+        assertEquals(2, SkillDemo1.multiplication(2, 3));
+    }
+}
